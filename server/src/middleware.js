c175c02942e3132863f7ea7da7e4cebc43e1e5f4
Or Perry <or@orperry.com>
2020-09-27 21:19:37 +0300

    Unable to fetch users bug fix

--- server/src/middleware.js
+++ server/src/middleware.js
@@ -3,10 +3,10 @@ function loginOnlyRoute(req, res, next) {
     console.log(req.cookies.login);
     if (req.cookies && req.cookies.login && req.cookies.login === '1') {
         // is logged in
-        next(new Error('need to login first'));
+        next();
     } else {
         // not logged in
-        next();
+        next(new Error('need to login first'));
     }
 }
 
